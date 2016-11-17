531d7d20047abd447e95ac301170b2dc87a89be9
Sam Rodens <sr.odens@gmail.com>
2016-11-16 23:01:01 -0500

    add hw3

---
+++ angular-module3-solution/app.js
@@ -0,0 +1,82 @@
+(function () {
+  'use strict';
+
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .directive('foundItems', FoundItemsDirective)
+  .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItemsList.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      },
+      bindToController: false,
+      controllerAs: 'menu'
+    };
+      //  controllerAs: 'foundItems',
+
+
+
+    return ddo;
+  }
+  function FoundItemsDirectiveController() {
+    var menu = this;
+
+    return true;
+  }
+
+
+
+
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var menu = this;
+    menu.foundItems=[];
+
+    menu.matchMenuItem = function (searchTerm) {
+      if (searchTerm){
+        menu.foundItems= MenuSearchService.getMatchedMenuItems(searchTerm);
+      }
+    };
+
+    menu.removeItem= function (index) {
+      menu.foundItems.splice(index,1);
+
+    };
+
+  }
+
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedMenuItems = function (searchTerm) {
+      var foundItems =[];
+      $http({
+        method: "GET",
+        url: (ApiBasePath + "/menu_items.json"),
+        // params: {
+        //   category: shortName
+        // }
+      }).then(function(response){
+
+        for(var i =0; i<response.data.menu_items.length; i++){
+          if (response.data.menu_items[i].description.match(searchTerm)){
+            foundItems.push(response.data.menu_items[i]);
+          }
+        }
+
+      });
+      return foundItems;
+
+
+    };
+
+  }
+
+})();
